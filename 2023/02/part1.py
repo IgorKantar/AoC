48ce374720d904ad64e7d8ce8e463fa515938110
Igor Kantar <igor_kan_bl@hotmail.com>
2023-12-02 12:15:11 +0100

    add comments

--- 2023/02/part1.py
+++ 2023/02/part1.py
@@ -4,6 +4,7 @@ COLORS = [
     "blue"
 ]
 class Colors():
+    """Class representing colors used in the game."""
     def __init__(self, red=0, green=0, blue=0) -> None:
         self.red = red
         self.green = green
@@ -14,7 +15,9 @@ class GameColors(Colors):
         super().__init__(red, green, blue)
 
     def is_possible(self) -> bool:
+        # flag tuple
         r, g, b = (False,False,False)
+        # color class repring max number of cubes
         MAX_CUBE_SET = Colors(12, 13, 14)
         if self.red <= MAX_CUBE_SET.red:
             r = True
@@ -22,23 +25,30 @@ class GameColors(Colors):
             g = True
         if self.blue <= MAX_CUBE_SET.blue:
             b = True
+        # return False if any cube colors are over max allowed
         for flag in [r,g,b]:
             if not flag:
                 return False
         return True
 
 def get_lines() -> list:
+    # return list of lines
     with open("input.txt") as f:
         return f.readlines()
 
 def get_start_index(s: str, ss: str):
+    # return starting index of substring
     return s.find(ss)
 
 def get_number(s: str, ss: str):
+    # return integer value of hand color
+    # ex. "8 green" -> "8" -> 8
     s = s.replace(f" {ss}", "")
     return int(s)
 
 def has_impossible(game_hands: list):
+    # game_hands is list of bools
+    # return True if game contains impossible hand
     return game_hands.__contains__(False)
 
 if __name__ == "__main__":
@@ -46,23 +56,38 @@ if __name__ == "__main__":
     games = get_lines()
 
     for id_, game in enumerate(games):
+        # failed my first submit bc i forgot this
         id_ += 1
+        # forgot to give better name
         x = get_start_index(game, ":")
+        # get game line without game id
         game = game[x+2:]
+        # should have named this hands, get list
+        # of hands in game and strip them
         sets = game.split(";")
         sets = [set_.strip() for set_ in sets]
+        # 'hand possible list', list of bools that
+        # will represent whether hand is possible
         hand_poss_list = []
         for hand in sets:
+            # init hand colors
             hand_colors = GameColors()
+            # make list of hand colors and strip whitespace
             hand = hand.split(",")
             hand = [t.strip() for t in hand]
             for color in hand:
                 for col in COLORS:
+                    # if color in hand
                     if col in color:
+                        # get the int value
                         num = get_number(color, col)
+                        # set color attribute, ex. equivalent
+                        # to hand_colors.green = 20
                         hand_colors.__setattr__(col, num)
+            # append hand possible flag
             hand_poss_list.append(hand_colors.is_possible())
 
+        # if all flags True, add id to sum
         if not has_impossible(hand_poss_list):
             sum_ += id_
 
--- 2023/02/part2.py
+++ 2023/02/part2.py
@@ -8,6 +8,11 @@ from part1 import (
 )
 
 class MinPossibleColors(Colors):
+    """
+    Class that represents minimum required color cubes,
+    for all hands in a game to be possible.
+    """
+
     def __init__(self, red=0, green=0, blue=0) -> None:
         super().__init__(red, green, blue)
 
@@ -15,6 +20,8 @@ class MinPossibleColors(Colors):
         for color in COLORS:
             min_col  = self.__getattribute__(color)
             hand_col = hand.__getattribute__(color)
+            # if hand color value larget,
+            # set as new minimum posible
             if min_col < hand_col:
                 self.__setattr__(color, hand_col)
 
@@ -23,21 +30,34 @@ if __name__ == "__main__":
     games = get_lines()
 
     for id_, game in enumerate(games):
+        # failed my first submit bc i forgot this
         id_ += 1
+        # forgot to give better name
         x = get_start_index(game, ":")
+        # get game line without game id
         game = game[x+2:]
+        # should have named this hands, get list
+        # of hands in game and strip them
         sets = game.split(";")
         sets = [set_.strip() for set_ in sets]
+        # 'hand possible list', list of bools that
+        # will represent whether hand is possible
         hand_poss_list = []
         min_req_colors = MinPossibleColors()
         for hand in sets:
+            # init hand colors
             hand_colors = GameColors()
+            # make list of hand colors and strip whitespace
             hand = hand.split(",")
             hand = [t.strip() for t in hand]
             for color in hand:
                 for col in COLORS:
+                    # if color in hand
                     if col in color:
+                        # get the int value
                         num = get_number(color, col)
+                        # set color attribute, ex. equivalent
+                        # to hand_colors.green = 20
                         hand_colors.__setattr__(col, num)
             min_req_colors.add_hand(hand_colors)
 
